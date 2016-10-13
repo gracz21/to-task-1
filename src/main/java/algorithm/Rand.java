beb961d59873b6bf672cee8bc5fd296b42b62821
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-10-13 20:30:20 +0200

    Refactored random algorithm

--- src/main/java/algorithm/Rand.java
+++ src/main/java/algorithm/Rand.java
@@ -6,47 +6,49 @@ import model.Vertex;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 /**
  * Created by inf109714 on 11.10.2016.
  */
-public class Rand {
-    private List<Vertex> vertices;
-    private Result result;
+public class Rand extends Algorithm {
 
     public Rand(Graph graph) {
-        this.vertices = graph.getVertices();
-        this.result = new Result();
+        super(graph);
     }
 
     public void executeAlgorithm() {
         int currentSolutionValue;
         List<Integer> currentSolution = new LinkedList<>();
-        for(Vertex vertex: vertices) {
+        for(Vertex vertex: this.vertices) {
             currentSolutionValue = 0;
             currentSolution.clear();
-            currentSolution.add(vertices.indexOf(vertex));
+            currentSolution.add(this.vertices.indexOf(vertex));
 
             while(currentSolution.size() < 50) {
-                selectNextEdge(currentSolution);
+                nextIteration(currentSolution);
             }
 
             for(int i = 0; i + 1 < currentSolution.size(); i++) {
                 int index = i;
-                currentSolutionValue += vertices.get(currentSolution.get(i)).getEdges().stream()
+                currentSolutionValue += this.vertices.get(currentSolution.get(i)).getEdges().stream()
                         .filter(edge -> edge.getEndVertexNumber() == currentSolution.get(index+1))
                         .findFirst().get().getCost();
             }
 
-            result.updateResult(currentSolution, currentSolutionValue);
+            this.result.updateResult(currentSolution, currentSolutionValue);
         }
     }
 
-    private void selectNextEdge(List<Integer> currentSolution) {
-        List<Edge> availableEdges = vertices.get(currentSolution.get(currentSolution.size() - 1)).getEdges().stream()
+    @Override
+    protected void nextIteration(List<Integer> currentSolution) {
+        List<Edge> availableEdges = this.vertices.get(currentSolution.get(currentSolution.size() - 1))
+                .getEdges()
+                .stream()
                 .filter(e -> !currentSolution.contains(e.getEndVertexNumber()))
                 .collect(Collectors.toList());
-        currentSolution.add(availableEdges.get((new java.util.Random()).nextInt(availableEdges.size() - 1)).getEndVertexNumber());
+        int index = new Random().nextInt(availableEdges.size() - 1);
+        currentSolution.add(availableEdges.get(index).getEndVertexNumber());
     }
 }
